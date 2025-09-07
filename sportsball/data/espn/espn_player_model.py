3da7fd9e1e5c597e942c5efdf96fbd3c9adeb619
Will Sackfield <will.sackfield@gmail.com>
2025-09-07 15:19:29 -0400

    Add more locations

--- sportsball/data/espn/espn_player_model.py
+++ sportsball/data/espn/espn_player_model.py
@@ -30,6 +30,7 @@ _BAD_URLS = {
     "http://sports.core.api.espn.com/v2/sports/football/leagues/college-football/seasons/2025/athletes/4693647?lang=en&region=us",
     "http://sports.core.api.espn.com/v2/sports/basketball/leagues/mens-college-basketball/seasons/2025/athletes/4433138?lang=en&region=us",
     "http://sports.core.api.espn.com/v2/sports/basketball/leagues/mens-college-basketball/seasons/2025/athletes/4703879?lang=en&region=us",
+    "http://sports.core.api.espn.com/v2/sports/basketball/leagues/mens-college-basketball/seasons/2025/athletes/4702946?lang=en&region=us",
 }
 
 
