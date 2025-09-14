1411ea31bce56b84a0ab41c5c0cb68b75b30c891
Will Sackfield <will.sackfield@gmail.com>
2025-09-14 11:46:35 -0400

    Check for value in state for short handed time on ice

--- sportsball/data/espn/espn_player_model.py
+++ sportsball/data/espn/espn_player_model.py
@@ -2795,9 +2795,10 @@ def _create_espn_player_model(
                                     power_play_time_on_ice, stat["value"]
                                 )
                         elif stat["name"] == "shortHandedTimeOnIce":
-                            short_handed_time_on_ice = more_interesting(
-                                short_handed_time_on_ice, stat["value"]
-                            )
+                            if "value" in stat:
+                                short_handed_time_on_ice = more_interesting(
+                                    short_handed_time_on_ice, stat["value"]
+                                )
                         elif stat["name"] == "evenStrengthTimeOnIce":
                             even_strength_time_on_ice = more_interesting(
                                 even_strength_time_on_ice, stat["value"]
